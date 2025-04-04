7ffda2033e42602b0fe5266f05bee00a6b723a6e
WanisC <wanischouaib1@gmail.com>
2025-04-04 20:46:27 +0200

    Adding log folder check/creation

--- src/writing/file.h
+++ src/writing/file.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 
+int CheckLogFolder(const char *chemin);
+void CreateLogFolder(const char *chemin);
 void WriteIntoFile(char data, const char* filePath);
 void WriteSpecialKeyIntoFile(char *data, const char* filePath);
